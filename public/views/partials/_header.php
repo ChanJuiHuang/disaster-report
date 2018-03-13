37c7f91f15c22a6558c706b6437f8f86b05460c6
Chan Jui Huang <ray81415@gmail.com>
2018-03-13 11:52:24 +0800

    What:把html header獨立出來成一份檔案
    Why:
    方便管理

---
+++ public/views/partials/_header.php
@@ -0,0 +1,7 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/public/bootstrap-v4.0.0/css/bootstrap.min.css">
+  <title>Disaster Report</title>
+</head>
