45f4d6c67e6328c924d0a205bfa2f5541d494f90
Chan Jui Huang <ray81415@gmail.com>
2018-03-13 11:54:00 +0800

    What:把html navbar獨立成一份檔案
    Why:
    方便管理

---
+++ public/views/partials/_navbar.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-3" style="background-color: #e3f2fd;">
+    <a class="navbar-brand">彰化縣消防局災情查報系統</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <!-- <ul class="navbar-nav mr-auto"> -->
+            <!-- <li class="nav-item">
+                <a class="nav-link" href="/">Home
+                    <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/contact">Contact</a>
+            </li> -->
+        <!-- </ul> -->
+        
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
+                    羅旭辰
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="">Posts</a>
+                    <a class="dropdown-item" href="">Categories</a>
+                    <a class="dropdown-item" href="">Tags</a>
+                    <div class="dropdown-divider"></div>
+                    <form action="/routes/auth/logout.php" method="POST">
+                        <button class="dropdown-item" type="submit" style="curs-or: pointer;">Logout</button>
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
