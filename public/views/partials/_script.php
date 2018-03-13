1d303e503e3520e5f47cbe70e66f396da17471b3
Chan Jui Huang <ray81415@gmail.com>
2018-03-13 15:41:08 +0800

    What:將js file分離出來
    Why:
    方便管理

---
+++ public/views/partials/_script.php
@@ -0,0 +1,3 @@
+<script src="/disaster_report/public/bootstrap-v4.0.0/js/jquery-3.2.1.slim.min.js"></script>
+<script src="/disaster_report/public/bootstrap-v4.0.0/js/popper.min.js"></script>
+<script src="/disaster_report/public/bootstrap-v4.0.0/js/bootstrap.min.js"></script>
