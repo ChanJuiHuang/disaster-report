0e2bddf5016418449d4766f1ae3bd4b2adbab33e
Chan Jui Huang <ray81415@gmail.com>
2018-03-14 09:37:32 +0800

    What:新增圖形驗證碼的輸入欄位，修改login表單的寬度
    Why:
    增加安全性
    
    How:
    新增input欄位

--- index.php
+++ index.php
@@ -25,22 +25,31 @@ session_write_close();
   <?php include($_SERVER['DOCUMENT_ROOT'] . '/public/views/partials/_navbar.php'); ?>
   <div class="container">
     <div class="row">
-      <div class="col-md-4 offset-md-4">
+      <div class="col-md-6 offset-md-3">
           <div class="card">
-              <!-- <div class="card-title card-header">登入</div> -->
+              <div class="card-title card-header" style="font-size: 6vmin; text-align: center;">登入</div>
               <div class="card-body">
                   <form method="POST" action="/routes/auth/login.php">
 
                       <div class="form-group">
                         <label for="account" class="">臂章號碼：</label>
-                        <input id="account" type="text" class="form-control" name="account" >
+                        <input id="account" type="text" class="form-control" name="account" required>
                       </div>
 
                       <div class="form-group">
                         <label for="password">密碼：</label>
-                        <input id="password" type="password" class="form-control" name="password" >
+                        <input id="password" type="password" class="form-control" name="password" required>
                       </div>
 
+                      <div class="form-group">
+						<label for="captcha">驗證碼：</label>
+						<p>
+                            <input type="text" name="captcha" id="captcha" class="form-control">
+						    <img src="/modules/showCaptcha.php" id="captcha-img">
+                            <a href="#" id="reload-captcha" style="font-size: 12px">重新產生</a>
+                        </p>
+                      </div>
+                      
                       <div class="form-group">
                         <button type="submit" class="btn btn-primary btn-block">
                             登入
@@ -54,8 +63,6 @@ session_write_close();
   </div>
 </body>
 
-<script src="/public/bootstrap-v4.0.0/js/jquery-3.2.1.slim.min.js"></script>
-<script src="/public/bootstrap-v4.0.0/js/popper.min.js"></script>
-<script src="/public/bootstrap-v4.0.0/js/bootstrap.min.js"></script>
-<script src="/public/js/index.js"></script>
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/public/views/partials/_script.php'); ?>
+
 </html>
