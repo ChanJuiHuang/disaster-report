6acb59e86d76b185d83e29992a815ea4fbb80b35
Chan Jui Huang <ray81415@gmail.com>
2018-03-16 09:25:43 +0800

    What:在header file加入mainMenu.css

--- public/views/partials/_header.php
+++ public/views/partials/_header.php
@@ -3,5 +3,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="/disaster_report/public/bootstrap-v4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/disaster_report/public/css/mainMenu.css">
   <title>Disaster Report</title>
 </head>
