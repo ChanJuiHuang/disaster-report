5032fc3e9b1bdc45f9f58377976c6a66b7424ce7
Chan Jui Huang <ray81415@gmail.com>
2018-03-16 09:22:31 +0800

    What:加入main menu page
    Why:
    製作主選單
    
    How:
    主頁面有四大按鈕，分別是
    1. 豪大雨災情查報
    2. 烏溪、濁水溪災情查報
    3. 地震災情查報
    4.其他災情查報

---
+++ public/views/mainMenu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/public/views/partials/_header.php'); ?>
+
+<body>
+
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/public/views/partials/_navbar.php'); ?>
+  <div class="container">
+    <div class="d-flex justify-content-center">
+      <button class="mainmenu__button m-1">豪大雨</br>災情查報</button>
+      <button class="mainmenu__button m-1">烏溪、濁水溪</br>災情查報</button>
+    </div>
+    <div class="d-flex justify-content-center">
+      <button class="mainmenu__button m-1">地震</br>災情查報</button>
+      <button class="mainmenu__button m-1">其他</br>災情查報</button>
+    </div>
+  </div>
+
+</body>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/public/views/partials/_script.php'); ?>
+
+</html>
