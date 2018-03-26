124700f55d3c28a914d2821657df2dc5e013f4f3
Chan Jui Huang <ray81415@gmail.com>
2018-03-26 10:16:35 +0800

    What:新增topics的store route
    How:
    儲存topic和working team進table

--- routes/topics/index.php
+++ routes/topics/index.php
@@ -1,11 +1,22 @@
 <?php
-  require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/modules/session.php');
-  require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/modules/isLogin.php');
 
-  session();
-  if (!$isLogin()) {
-    header('Location: /disaster_report/index.php');
-  }
+require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/modules/session.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/modules/isLogin.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/config/db_config.php');
+
+session();
+if (!$isLogin()) {
+  header('Location: /disaster_report/index.php');
+}
+
+try {
+  $conn = new PDO($dsn, $db_user, $db_password);
+  $topicsStmt = $conn->prepare('SELECT name, created_at FROM topics ORDER BY created_at;');
+  $topicsStmt->execute();
+  $topics = $topicsStmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (Exception $e) {
+  header("Location: /disaster_report/routes/auth/logout.php?fail={$e->getCode()}");
+}
 ?>
 
 <!DOCTYPE html>
@@ -25,7 +36,7 @@
               <div class="col-md-10 offset-md-1">
                 <?php if ($_SESSION['is_center']) { ?>
                 <a href="/disaster_report/routes/topics/create.php<?= isset($_SERVER['QUERY_STRING']) ? '?'.$_SERVER['QUERY_STRING'] : '' ?>">
-                  <button type="button" class="btn btn-primary">新增主題</button>
+                  <button type="button" class="btn btn-secondary">新增主題</button>
                 </a>
                 <?php } ?>
               </div>
@@ -40,10 +51,12 @@
                     </tr>
                   </thead>
                   <tbody>
+                    <?php foreach ($topics as $topic) { ?>
                     <tr>
                       <th scope="row"><?= $topic['name'] ?></th>
                       <td><?= date('Y-m-d H:i:s', strtotime($topic['created_at'])) ?></td>
                     </tr>
+                    <?php } ?>
                   </tbody>
                 </table>
               </div>
---
+++ routes/topics/store.php
@@ -0,0 +1,47 @@
+<?php
+
+require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/modules/session.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/modules/isLogin.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/config/db_config.php');
+
+session();
+
+if (!$isLogin()) {
+  header('Location: /disaster_report/index.php');
+}
+
+if (empty($_POST['topic'])) {
+  header("Location: /disaster_report/routes/topics/create.php?type={$_POST['type']}");
+  session_flash('error', '請填寫主題！');
+} elseif (empty($_POST['type'])) {
+  header("Location: /disaster_report/routes/topics/create.php?type={$_POST['type']}");
+} elseif (empty($_POST['teams'])) {
+  header("Location: /disaster_report/routes/topics/create.php?type={$_POST['type']}");
+  session_flash('error', '請選填出勤分隊！');
+} else {
+  $topic = null;
+  $type = null;
+  $teams = null;
+  extract($_POST, EXTR_IF_EXISTS);
+
+  try {
+    $conn = new PDO($dsn, $db_user, $db_password);
+    $conn->beginTransaction();
+    $topicsStmt = $conn->prepare('INSERT INTO topics(name, type) VALUES (?, ?)');
+    $topicsStmt->execute([$topic, $type]);
+    $topic_id = $conn->lastInsertId();
+
+    $placeholders = [];
+    for ($i=0; $i < count($teams); $i++) { 
+      $placeholders[] = "(?, {$topic_id})";
+    }
+    $teamsStmt = $conn->prepare("INSERT INTO working_teams(team_id, topic_id) VALUES " . join(',', $placeholders));
+    $teamsStmt->execute($teams);
+    $conn->commit();
+
+    header("Location: /disaster_report/routes/topics/index.php?type={$type}");
+  } catch (Exception $e) {
+    header("Location: /disaster_report/routes/topics/create.php?type={$_POST['type']}&fail={$e->getCode()}");
+    session_flash('error', '伺服器連線錯誤，請登出並重新整理頁面，再次登入！');
+  }
+}
