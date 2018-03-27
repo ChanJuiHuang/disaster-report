d7e7f67bb97431760b2c7feb8f81c50b25eee8a1
Chan Jui Huang <ray81415@gmail.com>
2018-03-27 10:33:30 +0800

    What:將顯示error message的方式從js alert（用query string選擇alert）改成session flash
    Why:
    fail code會隨著exception增加，管理不易
    
    How:
    使用自製的session flash

--- routes/auth/login.php
+++ routes/auth/login.php
@@ -2,11 +2,20 @@
 
 require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/config/db_config.php');
 require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/modules/session.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/modules/isLogin.php');
+
+session();
+
+if ($isLogin()) {
+  header('Location: /disaster_report/public/views/mainMenu.php');
+}
 
 if (empty($_POST['account']) || empty($_POST['password'])) {
-  header('Location: /disaster_report/index.php?fail=1');
+  header('Location: /disaster_report/index.php');
+  session_flash('error', '您的帳號或密碼輸入錯誤！');
 } elseif (empty($_POST['captcha'])) {
-  header('Location: /disaster_report/index.php?fail=2');
+  header('Location: /disaster_report/index.php');
+  session_flash('error', '您的驗證碼輸入錯誤！');
 } else {
   $account = null;
   $password = null;
@@ -19,18 +28,14 @@ if (empty($_POST['account']) || empty($_POST['password'])) {
     $userStmt->execute([$account, $password]);
     $user = $userStmt->fetch(PDO::FETCH_ASSOC);
 
-    session();
-    if (!$conn) {
-      throw new Exception("Connection Error!", 0);
-    }
     if (!(($userStmt->rowCount() === 1) && $user)) {
-      throw new Exception("Account or Password is wrong!", 1);
+      throw new Exception("您的帳號或密碼輸入錯誤！", 1);
     }
     if ($captcha !== $_SESSION['captcha']) {
-      throw new Exception("Captcha is wrong!", 2);
+      throw new Exception("您的驗證碼輸入錯誤！", 2);
     }
     if ($_COOKIE['x_csrf_token'] !== $_SESSION['csrfToken']) {
-      throw new Exception("CSRF Token is wrong!", 3);
+      throw new Exception(null, 3);
     }
     $_SESSION['name'] = $user['memname'];
     $_SESSION['account'] = $account;
@@ -63,7 +68,11 @@ if (empty($_POST['account']) || empty($_POST['password'])) {
     // $_SESSION['team_NO'] = $user['UnitID'];
     header('Location: /disaster_report/public/views/mainMenu.php');
   } catch (Exception $e) {
-    header('Location: /disaster_report/index.php?fail=' . $e->getCode());
+    if ($e->getCode() !== 7) {
+      header('Location: /disaster_report/index.php');
+      session_flash('error', $e->getMessage());
+    } else {
+      header("Location: /disaster_report/routes/auth/logout.php?fail={$e->getCode()}");
+    }
   }
 }
-session_write_close();
