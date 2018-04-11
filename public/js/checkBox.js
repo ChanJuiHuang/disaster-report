f6cd28da49c3e0bdf4cd2d33cdb23156b1361ca3
Chan Jui Huang <ray81415@gmail.com>
2018-04-11 09:05:50 +0800

    What:修正check box反選功能
    Why:
    原功能只會取消全部勾選項目
    
    How:
    用not邏輯反向選取

--- public/js/checkBox.js
+++ public/js/checkBox.js
@@ -1,10 +1,14 @@
-const checkBox = function (pattern, isCheck) {
+const checkBox = function (pattern, option) {
   let inputs = document.getElementsByTagName('input')
   let reg = null
   for (let index = 0; index < inputs.length; index++) {
     reg = new RegExp(pattern, 'g')
     if (reg.test(inputs[index].id)) {
-      inputs[index].checked = isCheck
+      if (option === 'checkAll') {
+        inputs[index].checked = true
+      } else if (option === 'reverseCheck') {
+        inputs[index].checked = !inputs[index].checked
+      }
     }
   }
 }
@@ -14,11 +18,11 @@ let unCheckTeams = document.getElementsByClassName('unCheckTeams')
 let teams = ['A', 'B', 'C', 'D', 'E']
 for (let index = 0; index < checkTeams.length; index++) {
   checkTeams[index].addEventListener('click', function (event) {
-    checkBox(teams[index], true)
+    checkBox(teams[index], 'checkAll')
     event.preventDefault()
   })
   unCheckTeams[index].addEventListener('click', function (event) {
-    checkBox(teams[index], false)
+    checkBox(teams[index], 'reverseCheck')
     event.preventDefault()
   })
 }
