f1054aa5259f51acc26c0c080252b48b93d14f06
Chan Jui Huang <ray81415@gmail.com>
2018-04-11 09:19:49 +0800

    What:新增update router of topics，用來更新topic name

---
+++ routes/topics/update.php
@@ -0,0 +1,38 @@
+<?php
+
+require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/modules/session.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/modules/isLogin.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/disaster_report/config/db_config.php');
+
+session();
+
+if (!$isLogin()) {
+  header('Location: /disaster_report/index.php');
+  return;
+}
+
+if (isset($_SERVER['QUERY_STRING'])) {
+  parse_str($_SERVER['QUERY_STRING'], $queryString);
+}
+
+if (empty($_POST['topic'])) {
+  header("Location: /disaster_report/routes/topics/create.php?type={$_POST['type']}");
+  session_flash('error', '請填寫主題！');
+} else {
+  $topic = null;
+  extract($_POST, EXTR_IF_EXISTS);
+
+  try {
+    if ($_COOKIE['x_csrf_token'] !== $_SESSION['csrfToken']) {
+      throw new Exception(null, 3);
+    }
+    $conn = new PDO($dsn, $db_user, $db_password);
+    $topicStmt = $conn->prepare("UPDATE topics SET name = '{$topic}' WHERE id = {$queryString['topic_id']}");
+    $topicStmt->execute();
+    header("Location: /disaster_report/routes/topics/show.php?topic_id={$queryString['topic_id']}");
+  } catch (Exception $e) {
+    header("Location: /disaster_report/routes/auth/logout.php?fail={$e->getCode()}");
+  }
+}
+
+?>
